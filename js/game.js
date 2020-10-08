684d78ca6423771c4f14dec49f6e6db941ec4a26
Vitaliy-b-s <butaji9i54@gmail.com>
2020-10-08 22:45:38 +0300

    elements swap added

--- js/game.js
+++ js/game.js
@@ -1,44 +1,77 @@
 // Исходный массив
-let defoultGame = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const fieldSize = 4;
+let defaultGame = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let emptyIndex;
 
 //Фунция перемешивания массива
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 // Функция отрисовки перемешанного массива
 function renderCells(array) {
-  for (let i = 0; i <= 15; i++) {
-    document.querySelector(`[data-id="${i}"]`).innerHTML = array[i];
-    if (array[i] === 0) {
-      document.querySelector(`[data-id="${i}"]`).style.border = "none";
-      document.querySelector(`[data-id="${i}"]`).innerHTML = " ";
+
+    for (let i = 0; i <= 15; i++) {
+        const selectedElement = document.querySelector(`[data-id="${i}"]`);
+        selectedElement.innerHTML = array[i];
+        if (array[i] === 0) {
+            selectedElement.style.border = "none";
+            selectedElement.innerHTML = " ";
+        }
     }
-  }
 }
 
 //Функция замены элементов в массиве
-function replaceElememt(event) {
-  let gameSize = 4;
-  let emptyCellRow;
-  let emptyCellColumn;
-  let emtyCell;
-  let clickedCell = event.target.dataset.id;
+function onCellClick(event) {
+    const targetIndex = event.target.dataset.id;
+    const canSwap = canSwapElements(emptyIndex, targetIndex);
+    if (canSwap) {
+        swapElements(shuffledArray, targetIndex, emptyIndex);
+        renderCells(shuffledArray)
+        emptyIndex = findEmpty();
 
-  for (let i = 0; i <= 15; i++) {
-    if (shuffledArray[i] == 0) {
-      emtyCell = shuffledArray.indexOf(shuffledArray[i]);
     }
-  }
-
-  emptyCellRow = Math.floor(emtyCell / gameSize);
-  emptyCellColumn = emtyCell % gameSize;
 }
 
-let shuffledArray = shuffleArray(defoultGame);
-fillIngDivs(shuffledArray);
-document.querySelector(".app__wrap").addEventListener("click", replaceElememt);
+function findEmpty() {
+    return shuffledArray.findIndex(item => item == 0)
+}
+
+function canSwapElements(emptyIndex, targetIndex) {
+
+    function extractRowAndColumn(index) {
+        return {
+            row: Math.floor(index / fieldSize),
+            column: index % fieldSize
+        }
+    }
+
+    const empty = extractRowAndColumn(emptyIndex);
+    const target = extractRowAndColumn(targetIndex);
+
+    if ((empty.column === target.column && Math.abs(target.row - empty.row) == 1)) {
+        return true;
+    } else if ((empty.row === target.row && Math.abs(empty.column - target.column) == 1)) {
+        return true;
+    }
+
+    return false
+}
+
+function swapElements(array, firstIndex, secondIndex) {
+    let temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+
+}
+
+let shuffledArray = shuffleArray(defaultGame);
+emptyIndex = findEmpty()
+renderCells(shuffledArray);
+
+document.querySelectorAll('.game-cell')
+    .forEach(cell => cell.addEventListener('click', onCellClick))
